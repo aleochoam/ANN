dfbdbc5a6b92f0e63caaae630e0336d3c785578a
Juan Daniel Morales <moralesarias94@gmail.com>
2017-05-30 12:52:41 -0500

    Entrenamiento y verificacion

--- src/ann.py
+++ src/ann.py
@@ -3,12 +3,14 @@ import numpy as np
 class ANN(object):
   """Red Neuronal"""
   def __init__(self, arg):
-    self.entradas = 1
+    self.entradas = 2
     self.salidas = 2
-    self.tamCapaOculta = 3
-
+    self.tamCapaOculta = 4
+    print("Init")
     self.W1 = np.random.randn(self.entradas, self.tamCapaOculta)
+    print(self.W1)
     self.W2 = np.random.randn(self.tamCapaOculta, self.salidas)
+    print(self.W2)
 
   def tanh(self, x):
     return np.tanh(x)
@@ -17,7 +19,7 @@ class ANN(object):
     return 1-(self.tanh(x))**2
 
   def softMax(self, x):
-    return np.exp(x)
+    return self.softMax2(np.exp(x))
 
   def softMax2(self, x):
     sum = np.sum(x, axis=1)
@@ -34,21 +36,47 @@ class ANN(object):
   def devEcm(self, i, o):
     return o-i
 
+  def costFunction(self, x, y):
+    self.yHat = self.forwardProp(x)
+    print("yHat:\n", self.yHat)
+    delta3 = np.multiply(-(y-self.yHat), self.devSoftMax(self.z3))
+    #print("delta3\n",delta3)
+    dJdW2 = np.dot(self.a2.T, delta3)
+    delta2 = np.dot(delta3, self.W2.T)* self.devTanh(self.z2)
+    #print("delta2\n",delta2)
+    #print("devTanh\n",self.devTanh(self.z2))
+    dJdW1 = np.dot(x.T, delta2)
+    return dJdW1, dJdW2
+
   #Forward Propagation
   def forwardProp(self, input):
     self.z2 = np.dot(input, self.W1)
-    #print(self.z2)
+    #print("z2\n",self.z2)
     self.a2 = self.tanh(self.z2)
     #print(self.a2)
     self.z3 = np.dot(self.a2, self.W2)
     #print(self.z3)
     outp = self.softMax(self.z3)
-    outp1 = self.softMax2(outp)
-    return outp1
+    return outp
 
 def main():
   ann = ANN(1)
-  print(ann.forwardProp([[1],[2],[3]]))
+  #x = ann.forwardProp([[1],[2],[3]])
+  x = np.array([[1,3],[2,3],[6,8],[4,5],[4,6],[6,7]])
+  y = [[0,1],[1,0],[0,1],[1,0],[0,1],[1,0]]
+  #print("dJdW2:\n",ann.costFunction(x, y))
+  for i in range(10000):
+    dJdW1, dJdW2 = ann.costFunction(x,y)
+    #print("dJdW\n", dJdW1,"\n", dJdW2)
+    alpha = 0.01
+    ann.W1 = ann.W1 - alpha* dJdW1
+    ann.W2 = ann.W2 - alpha* dJdW2
+    #print("W1:\n",ann.W1)
+    #print("W2:\n",ann.W2)
+  a = [[11,7],[4,8],[14,5],[2,4],[7,9],[20,21]]
+  b = np.array([[0,1],[0,1],[1,0],[0,1],[0,1],[1,0]])
+  classify = ann.forwardProp(a)
+  print(classify)
   #ann.forwardProp([1,2,3])
 if __name__ == '__main__':
   main()
